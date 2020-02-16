3b58d502b2486f98d7af9b34712b02f0be02c0df
JmPotato <ghzpotato@gmail.com>
2020-02-16 17:49:16 +0800

    Basic vga buffer operations

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+mod vga_buffer;
+
+static HELLO: &[u8] = b"Hello World!";
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+    // panic!("Some panic message");
+
+    loop {}
+}
