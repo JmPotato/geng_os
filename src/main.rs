44ce4f1800a97d773aa64b275846c3201d43957e
JmPotato <ghzpotato@gmail.com>
2020-07-31 01:14:20 +0800

    First OS only shows 'Hello, world!'
    Signed-off-by: JmPotato <ghzpotato@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,28 @@
-#![no_std]
-#![no_main]
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
+
+extern crate rlibc;
 
 use core::panic::PanicInfo;
 
+/// This function is called on panic.
 #[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
+fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-mod vga_buffer;
+static HELLO: &[u8] = b"Hello, world!";
 
-static HELLO: &[u8] = b"Hello World!";
-
-#[no_mangle]
+#[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-    // panic!("Some panic message");
-
+    // this function is the entry point, since the linker looks for a function
+    // named `_start` by default
+    let vga_buffer = 0xb8000 as *mut u8;
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
     loop {}
 }
