81cd71e54966b2547e79b5b7431958316fac7dd8
JmPotato <ghzpotato@gmail.com>
2020-07-31 15:53:09 +0800

    Implement basic test framework
    Signed-off-by: JmPotato <ghzpotato@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,37 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(geng_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 extern crate rlibc;
 
 use core::panic::PanicInfo;
+use geng_os::println;
 
 /// This function is called on panic.
+#[cfg(not(test))]
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
-
-static HELLO: &[u8] = b"Hello, world!";
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
     // this function is the entry point, since the linker looks for a function
     // named `_start` by default
-    let vga_buffer = 0xb8000 as *mut u8;
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    println!("Hello World{}", "!");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
+}
+
+// our panic handler in test mode
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    geng_os::test_panic_handler(info)
 }
